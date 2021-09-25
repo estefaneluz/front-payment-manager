48b0a58cd01de3ee80c2ff7b634115dbabeb9b62
estefaneluz <estefanecortes@gmail.com>
2021-09-25 10:12:39 -0300

    formulario completo adicionado na pagina SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,36 @@
+import './styles.css';
+import '../../styles/form.css';
+import { useStyles } from '../../styles/material-ui-form';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import TextField from '@material-ui/core/TextField';
+import InputPassword from '../../components/InputPassword';
+
 function SignUp() {
+    const styles = useStyles();
+
     return(
-        <div>
-            
+        <div className="container-form">
+            <form className="form">
+                <img src={logo} alt="Logo da Cubos Academy" />
+                <TextField 
+                    id="standard-basic" 
+                    label="Nome" 
+                    className={styles.textField}
+                />
+                <TextField 
+                    id="standard-basic" 
+                    label="E-mail" 
+                    placeholder="exemplo@gmail.com"
+                    className={styles.textField}
+                />
+                <InputPassword id="password" label="Senha" />
+                <button className="btn btn-pink-opacity">Criar Conta</button>
+            </form>
+
+            <p>
+                Já possui uma conta? <Link to='/sign-in'>Acesse agora!</Link>
+            </p>
         </div>
     )
 }

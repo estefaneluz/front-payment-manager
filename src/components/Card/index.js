6995c29eec28a0adec4c9f446005bb1c395dab6f
estefaneluz <estefanecortes@gmail.com>
2021-09-25 18:57:51 -0300

    componente Card estilizado

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,10 @@
+import './styles.css';
+
 function Card({ children, icon, title }) {
     return(
         <div className="card-container">
             <div className="card-header">
-                <img src={icon} alt={`icone de ${title}`} />
+                <img src={icon} alt={`icone do card ${title}`} />
                 <p>{ title }</p>
             </div>
             <div className="card-content">

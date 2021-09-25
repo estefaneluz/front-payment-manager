7f02d634a3381d65bf49220f546d800d466c850c
estefaneluz <estefanecortes@gmail.com>
2021-09-25 18:07:36 -0300

    dropdown criado e estilizado em userMenu

--- src/components/MenuUser/index.js
+++ src/components/MenuUser/index.js
@@ -1,14 +1,27 @@
 import './styles.css';
 import userPicture from '../../assets/user-picture.jpg';
+import editIcon from '../../assets/edit-icon.svg';
+import logoutIcon from '../../assets/logout-icon.svg';
 
 function MenuUser() {
     return (
-        <div className="user-menu">
+        <div className="user-container">
             <img 
                 className="user-picture"
                 src={userPicture} 
                 alt="imagem do usuário" 
             />
+            <div className="user-dropdown">
+                <div>
+                    <img src={editIcon} alt="icone de editar" />
+                    <p>Editar</p>
+                </div>
+
+                <div>
+                    <img src={logoutIcon} alt="icone de deslogar" />
+                    <p>Deslogar</p>
+                </div>
+            </div>
         </div>
     );
 }

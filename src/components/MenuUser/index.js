cd45f0cde911e17855855ffd9f7edcb6e79f6c87
estefaneluz <estefanecortes@gmail.com>
2021-09-25 18:13:13 -0300

    toggle dropdown criado

--- src/components/MenuUser/index.js
+++ src/components/MenuUser/index.js
@@ -1,27 +1,37 @@
+import { useState, useEffect } from 'react';
 import './styles.css';
 import userPicture from '../../assets/user-picture.jpg';
 import editIcon from '../../assets/edit-icon.svg';
 import logoutIcon from '../../assets/logout-icon.svg';
 
 function MenuUser() {
+    const [dropdown, setDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdown(!dropdown);
+    }
+
     return (
         <div className="user-container">
             <img 
                 className="user-picture"
                 src={userPicture} 
                 alt="imagem do usuário" 
+                onClick={toggleDropdown}
             />
-            <div className="user-dropdown">
-                <div>
-                    <img src={editIcon} alt="icone de editar" />
-                    <p>Editar</p>
-                </div>
+            { dropdown &&
+                <div className="user-dropdown">
+                    <div>
+                        <img src={editIcon} alt="icone de editar" />
+                        <p>Editar</p>
+                    </div>
 
-                <div>
-                    <img src={logoutIcon} alt="icone de deslogar" />
-                    <p>Deslogar</p>
+                    <div>
+                        <img src={logoutIcon} alt="icone de deslogar" />
+                        <p>Deslogar</p>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 }

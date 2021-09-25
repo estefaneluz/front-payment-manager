5622acf34efcd23ccc4d0760043690ec0109261e
estefaneluz <estefanecortes@gmail.com>
2021-09-25 09:15:16 -0300

    adicionado: material-ui e elementos iniciais no sign-in

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,28 @@
 import './styles.css';
 import '../../styles/form.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 function SignIn() {
     return (
         <div className="container-form">
             <form className="form">
+                <img src={logo} alt="Logo da Cubos Academy" />
 
+                <TextField 
+                    id="standard-basic" 
+                    label="E-mail" 
+                    placeholder="exemplo@gmail.com" 
+                />
+
+                <button className="btn btn-pink-disabled">Entrar</button>
             </form>
+
+            <p>
+                Não tem uma conta? <Link to='/sign-up'>Cadastre-se</Link>
+            </p>
         </div>
     )
 }

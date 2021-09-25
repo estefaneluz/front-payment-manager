37bf614d8abba3b5999788fa02846b3582effa27
estefaneluz <estefanecortes@gmail.com>
2021-09-25 09:58:19 -0300

    add InputPassword e o useStyles no sign-in

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,23 +1,26 @@
 import './styles.css';
 import '../../styles/form.css';
+import { useStyles } from '../../styles/material-ui-form';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import InputPassword from '../../components/InputPassword';
 
 function SignIn() {
+    const styles = useStyles();
+
     return (
         <div className="container-form">
             <form className="form">
                 <img src={logo} alt="Logo da Cubos Academy" />
-
                 <TextField 
                     id="standard-basic" 
                     label="E-mail" 
-                    placeholder="exemplo@gmail.com" 
+                    placeholder="exemplo@gmail.com"
+                    className={styles.textField}
                 />
-
-                <button className="btn btn-pink-disabled">Entrar</button>
+                <InputPassword id="password" label="Senha" />
+                <button className="btn btn-pink-opacity">Entrar</button>
             </form>
 
             <p>

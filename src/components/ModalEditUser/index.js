d2e41d625fe9a82a040a905ce3c2a082ecf1ba2e
estefane <estefanecortes@gmail.com>
2021-09-27 22:55:50 -0300

    useForm adicionado na ModalEditUser

--- src/components/ModalEditUser/index.js
+++ src/components/ModalEditUser/index.js
@@ -1,10 +1,12 @@
 import './styles.css'
 import { useStyles } from '../../styles/form-material-ui';
+import { useForm } from 'react-hook-form';
 import TextField from '@material-ui/core/TextField';
 import InputPassword from '../../components/InputPassword';
 
 function ModalEditUser({ open, setOpen }) {
     const styles = useStyles();
+    const { register, handleSubmit } = useForm();
 
     const closeModal = () => {
         setOpen(!open);
@@ -26,27 +28,32 @@ function ModalEditUser({ open, setOpen }) {
                             id="standard-basic" 
                             label="Nome" 
                             className={styles.input}
+                            {...register('name', { required: true })}
                         />
                         <TextField 
                             id="standard-basic" 
                             label="E-mail" 
                             placeholder="exemplo@gmail.com"
                             className={styles.input}
+                            {...register('email', { required: true })}
                         />
                         <InputPassword 
                             id="password" 
                             label="Nova senha" 
-                            placeholder="Deixe vazio para não editar." 
+                            placeholder="Deixe vazio para não editar."
+                            register={register}
                         />
                         <TextField 
                             id="standard-basic" 
                             label="Telefone" 
                             className={styles.input}
+                            {...register('phone')}
                         />
                         <TextField 
                             id="standard-basic" 
                             label="CPF" 
                             className={styles.input}
+                            {...register('cpf')}
                         />
                         <button className={`btn btn-pink-opacity`}>
                             Editar conta

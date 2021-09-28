392fb9adf0109ab92ad1c50558612091601b5048
joaocdfarias <joaocdfariss@gmail.com>
2021-09-27 22:30:51 -0300

    mensagem de erro/sucesso adicionados na página SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -3,21 +3,28 @@ import '../../styles/form.css';
 
 import { useState, useEffect } from 'react';
 import { useStyles } from '../../styles/form-material-ui';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 import logo from '../../assets/logo.svg';
-import TextField from '@material-ui/core/TextField';
+import { TextField, Snackbar } from '@material-ui/core/';
+import { Alert } from '@material-ui/lab';
 import InputPassword from '../../components/InputPassword';
 
 function SignUp() {
     const styles = useStyles();
     const { register, watch, handleSubmit, formState: { errors } } = useForm();
     const [buttonClass, setButtonClass] = useState('-pink-opacity');
+    const [alert, setAlert] = useState({});
     const watchAllFields = watch();
+    const history = useHistory();
+
+    const clearAlert = () => setAlert({});
 
     const onSubmit = async data => {
-        await fetch('http://localhost:8000/cadastrar', {
+        clearAlert();
+
+        const response = await fetch('http://localhost:8000/cadastrar', {
             method: 'POST',
             mode: 'cors',
             cache: 'no-cache',
@@ -26,6 +33,21 @@ function SignUp() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
+        });
+
+        if(response.ok) {
+            setAlert({
+                type: 'success',
+                message: "Usuário cadastrado com sucesso!"
+            });
+            setTimeout(()=>history.push('/'), 4000);
+            return;
+        }
+
+        const message = await response.json();
+        setAlert({
+            type: 'error',
+            message
         });
     }
 
@@ -74,6 +96,17 @@ function SignUp() {
             <p>
                 Já possui uma conta? <Link to='/'>Acesse agora!</Link>
             </p>
+
+            {!!alert.message && 
+                <Snackbar 
+                    open={!!alert.message} 
+                    autoHideDuration={4000} 
+                    onClose={clearAlert}>
+                    <Alert onClose={clearAlert} severity={alert.type}>
+                        {alert.message}
+                    </Alert>
+                </Snackbar>
+            }
         </div>
     )
 }

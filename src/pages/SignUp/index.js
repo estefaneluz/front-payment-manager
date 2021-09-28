edde0db954217d532233ce2e9c795afd82d64774
joaocdfarias <joaocdfariss@gmail.com>
2021-09-27 21:03:43 -0300

    SignUp page: condição para classe do botão e inputs vermelhos quando há erro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,15 +1,20 @@
 import './styles.css';
 import '../../styles/form.css';
+
+import { useState, useEffect } from 'react';
 import { useStyles } from '../../styles/form-material-ui';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
 import logo from '../../assets/logo.svg';
 import TextField from '@material-ui/core/TextField';
 import InputPassword from '../../components/InputPassword';
-import { useForm } from 'react-hook-form';
 
 function SignUp() {
     const styles = useStyles();
-    const { register, handleSubmit } = useForm();
+    const { register, watch, handleSubmit, formState: { errors } } = useForm();
+    const [buttonClass, setButtonClass] = useState('-pink-opacity');
+    const watchAllFields = watch();
 
     const onSubmit = async data => {
         await fetch('http://localhost:8000/cadastrar', {
@@ -24,29 +29,46 @@ function SignUp() {
         });
     }
 
+    useEffect(() => {
+        if(!watchAllFields.name || !watchAllFields.password || !watchAllFields.email) {
+            setButtonClass('pink-opacity');
+        } else {
+            setButtonClass('pink');
+        }
+    }, [watchAllFields]);
+
     return(
         <div className="container-form">
             <form className="form" onSubmit={handleSubmit(onSubmit)}>
                 <img src={logo} alt="Logo da Cubos Academy" />
                 <TextField 
+                    error={!!errors.name}
                     id="name" 
                     label="Nome" 
                     className={styles.input}
-                    {...register('name')}
+                    {...register('name', { required: true })}
                 />
                 <TextField 
+                    error={!!errors.email}
                     id="email" 
                     label="E-mail" 
                     placeholder="exemplo@gmail.com"
+                    type="email"
                     className={styles.input}
-                    {...register('email')}
+                    {...register('email', { required: true })}
                 />
                 <InputPassword
+                    error={!!errors.password}
                     id="password"
                     label="Senha"
                     register={register}
                 />
-                <button className="btn btn-pink-opacity">Criar Conta</button>
+                <button 
+                    className={`btn btn-${buttonClass}`} 
+                    type='submit'
+                >
+                    Criar Conta
+                </button>
             </form>
 
             <p>

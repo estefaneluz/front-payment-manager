6ad7fb67cb0aed5cc82410be86e1885308dfc7e1
estefane <estefanecortes@gmail.com>
2021-09-28 17:52:25 -0300

    AuthContext criado e adicionado em routes

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,43 @@
+import { useState, useContext } from "react";
 import { 
     BrowserRouter as Router, 
     Route, 
-    Switch 
+    Switch,
+    Redirect 
 } from "react-router-dom";
+import { AuthContext } from "./contexts/authContext";
 
 import Main from './pages/Main';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from "./pages/Home";
 
-function Routes() {
+const ProtectedRoutes = (props) => {
+    const { token } = useContext(AuthContext);
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={SignIn} />
-                <Route path="/sign-up" component={SignUp} />
-                <Main>
-                    <Route path="/home" component={Home} />
-                </Main>
-            </Switch>
-        </Router>
+      <Route render={() => (token ? props.children : <Redirect to="/" />)} />
+    );
+};
+
+function Routes() {
+    const [token, setToken] = useState("");
+
+    const valueContext = {token, setToken}
+
+    return (
+        <AuthContext.Provider value={valueContext}>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={SignIn} />
+                    <Route path="/sign-up" component={SignUp} />
+                    <ProtectedRoutes>
+                        <Main>
+                            <Route path="/home" component={Home} />
+                        </Main>
+                    </ProtectedRoutes>
+                </Switch>
+            </Router>
+        </AuthContext.Provider>
     )
 }
 

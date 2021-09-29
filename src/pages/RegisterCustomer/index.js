c62b27c7149e989967a270b838fe7042c7faa58a
estefane <estefanecortes@gmail.com>
2021-09-29 11:54:58 -0300

    adicionando atributo type nos inputs e arrumando o arquivo

--- src/pages/RegisterCustomer/index.js
+++ src/pages/RegisterCustomer/index.js
@@ -6,28 +6,68 @@ function RegisterCostumer() {
         <div className="container-register-costumer">
             <h1>Adicionar Cliente</h1>
             <form className="form">
-                <InputCustomer label="Nome"/>
-                <InputCustomer label="E-mail"/>
+                <InputCustomer label="Nome" type="text" />
+                <InputCustomer label="E-mail" type="email" />
+
                 <div className="double-input">
-                    <InputCustomer label="CPF" classType="half" />
-                    <InputCustomer label="Telefone" classType="half" />
+                    <InputCustomer 
+                        label="CPF" 
+                        classType="half" 
+                        type="text" 
+                    />
+                    <InputCustomer 
+                        label="Telefone" 
+                        classType="half"
+                        type="text" 
+                    />
                 </div>
+
                 <div className="double-input">
-                    <InputCustomer label="CEP" classType="half" />
-                    <InputCustomer label="Logradouro" classType="half" />
+                    <InputCustomer 
+                        label="CEP" 
+                        classType="half" 
+                        type="text" 
+                    />
+                    <InputCustomer 
+                        label="Logradouro" 
+                        classType="half" 
+                        type="text" 
+                    />
                 </div>
+
                 <div className="double-input">
-                    <InputCustomer label="Bairro" classType="half" />
-                    <InputCustomer label="Cidade" classType="half" />
+                    <InputCustomer 
+                        label="Bairro" 
+                        classType="half" 
+                        type="text" 
+                    />
+                    <InputCustomer 
+                        label="Cidade" 
+                        classType="half" 
+                        type="text" 
+                    />
                 </div>
+
                 <div className="double-input">
-                    <InputCustomer label="Complemento" classType="half" />
-                    <InputCustomer label="Ponto de Referência" classType="half" />
+                    <InputCustomer 
+                        label="Complemento" 
+                        classType="half" 
+                        type="text" 
+                    />
+                    <InputCustomer 
+                        label="Ponto de Referência" 
+                        classType="half" 
+                        type="text" 
+                    />
                 </div>
 
                 <div className="flex-row column-gap-20">
-                    <button className="btn btn-border-pink">Cancelar</button>
-                    <button className="btn btn-pink-opacity">Adicionar Cliente</button>
+                    <button className="btn btn-border-pink">
+                        Cancelar
+                    </button>
+                    <button className="btn btn-pink-opacity">
+                        Adicionar Cliente
+                    </button>
                 </div>
             </form>
         </div>

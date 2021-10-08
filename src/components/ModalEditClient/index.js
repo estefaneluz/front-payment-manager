b1078dcaf334e7172edf50144989f78c94c11dc5
estefaneluz <estefanecortes@gmail.com>
2021-10-07 23:31:31 -0300

    criando ModalEditClient

---
+++ src/components/ModalEditClient/index.js
@@ -0,0 +1,204 @@
+import { useState, useEffect, useContext } from 'react';
+import InputClient from "../InputClient";
+import { useForm } from 'react-hook-form';
+
+import getAddressByCep from '../../services/viaCEP';
+import { GlobalStatesContext } from '../../contexts/GlobalStatesContext';
+
+function ModalEditClient() {
+    const [cep, setCep] = useState('');
+    const [city, setCity] = useState('');
+    const [district, setDistrict] = useState('');
+    const [street, setStreet] = useState('');
+    const [state, setState] = useState('');
+
+    const [buttonClass, setButtonClass] = useState('pink-opacity');
+
+    const { register, watch, handleSubmit, formState: { errors }, setValue } = useForm();
+    const watchFields = watch(['name', 'email', 'cpf', 'phone']);
+    const { setAlert } = useContext(GlobalStatesContext);
+
+    const clearFields = () => {
+        setCep('');
+        setCity('');
+        setDistrict('');
+        setStreet('');
+        setState('');
+
+        setValue('cpf', '');
+        setValue('phone', '');
+    }
+
+    const loadAddressByCep = async (cep) => {
+        const address = await getAddressByCep(cep);
+        
+        if(!address) {
+            setAlert({
+                type:"error",
+                message: "Endereço não encontrado"
+            });
+            return;
+        }
+
+        setCity(address.localidade);
+        setDistrict(address.bairro);
+        setStreet(address.logradouro);
+        setState(address.uf);
+    }
+
+    const onSubmit = () => {
+        console.log("oi");
+    }
+
+    useEffect(() => {
+
+        if(cep.length < 9 && city.length > 0) {
+            setCity('');
+            setDistrict('');
+            setStreet('');
+        }
+
+        if(cep.indexOf('-') !== -1) {
+            if(cep.length === 9) {
+                loadAddressByCep(cep)
+            }
+            return;
+        }
+
+        if(cep.length === 8) {
+            loadAddressByCep(cep)
+        }
+    }, [cep]);
+
+    useEffect(() => {
+        if(watchFields.includes('')) {
+            setButtonClass('pink-opacity');
+        } else {
+            setButtonClass('pink');
+        }
+    }, [watchFields]);
+
+    return(
+        <div className="modal">
+            <form 
+                className="form" 
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <InputClient
+                    id="name"
+                    label="Nome" 
+                    type="text"
+                    register={register}
+                    required={true}
+                    error={!!errors.name}
+                />
+                <InputClient
+                    id="email"
+                    label="E-mail" 
+                    type="email"
+                    register={register}
+                    required={true}
+                    error={!!errors.email}
+                />
+
+                <div className="double-input">
+                    <InputClient
+                        id="cpf"
+                        label="CPF" 
+                        classType="half" 
+                        type="text"
+                        mask="999.999.999-99"
+                        register={register}
+                        required={true}
+                        error={!!errors.cpf}
+                    />
+                    <InputClient
+                        id="phone"
+                        label="Telefone"
+                        mask="(99) 999999999"
+                        classType="half"
+                        type="text"
+                        register={register}
+                        required={true}
+                        error={!!errors.phone}
+                    />
+                </div>
+
+                <div className="double-input">
+                    <InputClient
+                        id="zipcode"
+                        label="CEP" 
+                        classType="half" 
+                        type="text"
+                        value={cep}
+                        onChange={(e) => setCep(e.target.value)}
+                        maxLength={9}
+                    />
+                    <InputClient
+                        id="street"
+                        label="Logradouro" 
+                        classType="half" 
+                        type="text" 
+                        value={street}
+                        onChange={(e) => setStreet(e.target.value)}
+                    />
+                </div>
+
+                <div className="double-input">
+                    <InputClient
+                        id="district"
+                        label="Bairro" 
+                        classType="half" 
+                        type="text"
+                        value={district}
+                        onChange={(e) => setDistrict(e.target.value)}
+                    />
+                    <InputClient 
+                        id="city"
+                        label="Cidade" 
+                        classType="half" 
+                        type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                </div>
+
+                <div className="double-input">
+                    <InputClient
+                        id="state"
+                        label="Estado" 
+                        classType="half" 
+                        type="text"
+                        value={state}
+                        onChange={(e) => setState(e.target.value)}
+                    />
+                    <InputClient 
+                        id="landmark"
+                        label="Ponto de Referência" 
+                        classType="half" 
+                        type="text"
+                        register={register}
+                    />
+                </div>
+
+                <InputClient
+                    id="additional"
+                    label="Complemento" 
+                    type="text"
+                    register={register}
+                />
+
+                <div className="flex-row column-gap-20">
+                    <button className="btn btn-border-pink" type="reset" onClick={clearFields}>
+                        Cancelar
+                    </button>
+                    <button className={`btn btn-${buttonClass}`} type="submit">
+                        Editar Cliente
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default ModalEditClient;
--- src/pages/Client/index.js
+++ src/pages/Client/index.js
@@ -1,6 +1,9 @@
 import './styles.css'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 import Table from '../../components/Table';
+import ModalEditClient from '../../components/ModalEditClient';
 import emailIcon from '../../assets/email-icon.svg';
 import phoneIcon from '../../assets/phone-icon.svg';
 import editIcon from '../../assets/edit-icon.svg';
@@ -13,37 +16,49 @@ const tableTitles = [
 ]
 
 function Client() {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div className="container"> 
-            <Link className="btn btn-border-pink" to="/clients/new"> 
-                Adicionar Cliente
-            </Link> 
-            <Table titles={tableTitles}>
-                <tr>
-                    <td className="table-client-data">
-                        <p className="table-client-name">Nome e Sobrenome da Cliente</p>
-                        <div className="table-client-contact">
-                            <div>
-                                <img src={emailIcon} alt="icone de e-mail"/>
-                                <p>email@email.com</p>
+        <>
+            <div className="container"> 
+                <Link className="btn btn-border-pink" to="/clients/new"> 
+                    Adicionar Cliente
+                </Link> 
+                <Table titles={tableTitles}>
+                    <tr>
+                        <td className="table-client-data">
+                            <p className="table-client-name">Nome e Sobrenome da Cliente</p>
+                            <div className="table-client-contact">
+                                <div>
+                                    <img src={emailIcon} alt="icone de e-mail"/>
+                                    <p>email@email.com</p>
+                                </div>
+                                <div>
+                                    <img src={phoneIcon} alt="icone de telefone"/>
+                                    <p>(71) 99999999</p>
+                                </div>
                             </div>
-                            <div>
-                                <img src={phoneIcon} alt="icone de telefone"/>
-                                <p>(71) 99999999</p>
+                        </td>
+                        <td>R$ 00.000,00</td>
+                        <td>R$ 00.000,00</td>
+                        <td>
+                            <div className="table-client-status text-red">
+                                <p> Inadimplente </p>
+                                <img 
+                                    src={editIcon} 
+                                    alt="Icone de editar cliente" 
+                                    onClick={() => setOpen(true)}
+                                />
                             </div>
-                        </div>
-                    </td>
-                    <td>R$ 00.000,00</td>
-                    <td>R$ 00.000,00</td>
-                    <td>
-                        <div className="table-client-status text-red">
-                            <p> Inadimplente </p>
-                            <img src={editIcon} alt="Icone de editar cliente"/>
-                        </div>
-                    </td>
-                </tr>
-            </Table>
-        </div>
+                        </td>
+                    </tr>
+                </Table>
+            </div>
+
+            {open && (
+                <ModalEditClient />
+            )}
+        </>
     )
 }
 

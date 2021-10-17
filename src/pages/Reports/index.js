95af3d6501f84f11ce52b016d44839bc100759ce
estefaneluz <estefanecortes@gmail.com>
2021-10-17 20:04:47 -0300

    ordenação na pagina de relatórios

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -4,16 +4,20 @@ import Table from '../../components/Table';
 import RowClient from "../../components/RowClient";
 import RowCharge from '../../components/RowCharge';
  import Search from '../../components/Search';
+
 import { clientTitles } from '../Client';
 import { chargesTitles } from '../Charges';
 import { GlobalStatesContext } from '../../contexts/GlobalStatesContext';
 import { ReportsStatesContext } from '../../contexts/ReportsStatesContext';
+import { OrderTableStatesContext } from '../../contexts/OrderTableStatesContext';
+import { sortDataByName, sortData } from '../../functions/sortDataByName';
 
 function Reports() {
     const [charges, setCharges] = useState([]);
     const [clients, setClients] = useState([]);
     const { token, setLoading } = useContext(GlobalStatesContext);
     const { reportFilter } = useContext(ReportsStatesContext);
+    const { orderTable } = useContext(OrderTableStatesContext);
 
     const handleStatusParam = () => {
         if(reportFilter.status === 'Em dia') return 'em-dia'
@@ -33,7 +37,12 @@ function Reports() {
             }
         );
         const response = await request.json();
+        response.sort(sortDataByName);
         setLoading(false);
+
+        if(orderTable === 'desc') {
+            return response.reverse();
+        }
 
         return response;
     }
@@ -52,7 +61,13 @@ function Reports() {
         );
 
         const response = await request.json();
+        response.sort(sortDataByName);
         setLoading(false);
+
+        if(orderTable === 'desc') {
+            return response.reverse();
+        }
+
         return response;
     }
 
@@ -69,6 +84,14 @@ function Reports() {
 
         awaitGetClients();
     }, [reportFilter]);
+
+    useEffect(() => {
+        if(reportFilter.page === 'Clientes') {
+            sortData(clients, setClients, orderTable);
+        } else {
+            sortData(charges, setCharges, orderTable);
+        }
+    }, [orderTable, reportFilter]);
 
     return(
         <div className="container">
